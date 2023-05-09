771bba0c682a2374234f2e75079e9dfebf75466d
Odimayo Taiye <odimayomoses6@gmail.com>
2023-05-09 14:12:15 +0100

    Landing Page Asusu, Please get back in case of any error, Thanks .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { DemoComponent } from './demo/demo.component';
+import { WhyusComponent } from './whyus/whyus.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "", component: HomeComponent
+  }, 
+  {
+    path: "about", component: AboutComponent
+  },
+  {
+    path: "demo", component: DemoComponent
+  },
+  {
+    path: "pricing", component: PricingComponent
+  },
+  {
+    path: "whyus", component: WhyusComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { AboutComponent } from './about/about.component';
+import { WhyusComponent } from './whyus/whyus.component';
+import { DemoComponent } from './demo/demo.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PricingComponent,
+    AboutComponent,
+    WhyusComponent,
+    DemoComponent,
+    FooterComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
